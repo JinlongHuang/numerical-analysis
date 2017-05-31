1b535f7d3443d6171f4be7b416e4cd1ac328ff49
Jinlong Huang <huangjl@mail.sustc.edu.cn>
2017-05-31 16:42:51 +0800

    Create Guass_SiedelIteration.m

---
+++ linearSystemEquation/Guass_SiedelIteration.m
@@ -0,0 +1,25 @@
+% Guass_Siedel iteration to solve a 3 x 3 linear system equation
+% A = [1,0,-1;-1/2,1,-1/4;1,-1/2,1]
+% b = [0.2;-1.425;2]
+% The accurate solution is [0.9;-0.8;0.7]
+
+Tg = [0,0,1;0,0,3/4;0,0,-5/8];
+
+invD_L = [1,0,0;1/2,1,0;-3/4,1/2,1];
+
+Cg = invD_L*[0.2;-1.425;2];
+
+xt = zeros(3,300);
+
+xt(:,1) = [0;0;0];
+
+xFin = [0.9;-0.8;0.7];
+
+for i = 2:300
+    xt(:,i) = Tg*xt(:,i-1)+Cg;
+    if (max(abs(xt(:,i)))-max(xFin))/max(xFin) <= 10^(-2)
+        fprintf(1, 'The result is : %4.3f\n',xt(:,i));
+        fprintf(1,'The number of iteration is : %d\n', i-1)
+        break;
+    end
+end
